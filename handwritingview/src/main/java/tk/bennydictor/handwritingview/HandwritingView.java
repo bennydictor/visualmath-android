da44658e78044af44c823e7e7807773110a43914
Michael Sorokin <benny@bennydictor.tk>
2018-03-04 15:19:18 +0300

    Add handwritingview subproject
    * Enable multidex
    * Bump min SDK to 23
    * Bump all outdated libraries to current versions

---
+++ handwritingview/src/main/java/tk/bennydictor/handwritingview/HandwritingView.java
@@ -0,0 +1,22 @@
+package tk.bennydictor.handwritingview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class HandwritingView extends View {
+    public HandwritingView(Context context) {
+        super(context);
+        init();
+    }
+
+    public HandwritingView(Context context, AttributeSet attributes) {
+        super(context, attributes);
+        init();
+    }
+
+    private void init() {
+        // TODO
+    }
+}
